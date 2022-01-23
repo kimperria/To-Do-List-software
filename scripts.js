47d0f51a7358404956a5b5dede2b001d497d4e0c
John-Kimani <kimwaweru100@gmail.com>
2022-01-23 14:50:59 +0300

    adds scripts.js, sets condition and functionality for both add and remove buttons

---
+++ scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('#add').click(function(){
+        let task = $('#new-task').val();
+        if (task.length == ''){
+            alert("Task name required");
+        }else{
+            $('.selected-item').append(`<li>${task}<i class="far remove fa-window-close"></i></li>`)
+            $('.remove').click(function(){
+               $(this).closest('li').remove(); 
+            });
+            $("#new-task").val(''); //allows you to add new task name
+        }
+    });
+
+    $('.clear').click(function(){
+        $('li').remove()
+    });
+});
